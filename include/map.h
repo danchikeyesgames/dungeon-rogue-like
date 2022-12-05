21c497f1b18b01848f008174eb2ee6aa77185e8c
danchikeyesgames <danchikhol@gmail.com>
2022-12-05 16:10:23 +0300

    add BSP-split

--- include/map.h
+++ include/map.h
@@ -16,7 +16,8 @@
 #define HEIGHTVIEW 25
 #define WIDTHVIEW  80
 
-#define MIN_LEAF_SIZE 2
+#define MIN_LEAF_SIZE 6
+#define MAX_LEAF_SIZE 20
 
 typedef struct _Map {
     cvector_t(room_ptr) rooms;
@@ -41,6 +42,7 @@ typedef struct _Leaf {
 void drawmap();
 leaf_t* leaf_create(int x, int y, int w, int h);
 bool leaf_split(leaf_ptr l);
+void BSP_split(void);
 
 
 
--- src/map.c
+++ src/map.c
@@ -22,19 +22,26 @@
 //     mvprintw(9, 1, map_room[7]);
 // }
 
+cvector_t(leaf_ptr) lvec;
+leaf_ptr leaf_root;
+
 leaf_t* leaf_create(int x, int y, int w, int h) {
     leaf_ptr newLeaf = (leaf_ptr) malloc(sizeof(leaf_t));
 
     newLeaf->x = x;
-    newLeaf->x = y;
-    newLeaf->x = w;
-    newLeaf->x = h;
+    newLeaf->y = y;
+    newLeaf->width = w;
+    newLeaf->height = h;
+    newLeaf->left = NULL;
+    newLeaf->right = NULL;
+    newLeaf->room = NULL;
 
     return newLeaf;
 }
 
 bool leaf_split(leaf_ptr l) {
     int max;
+    int plc_split;
     bool splitH;
     
     if (l->left != NULL && l->right != NULL) {
@@ -52,7 +59,44 @@ bool leaf_split(leaf_ptr l) {
     if (max < MIN_LEAF_SIZE)
         return false;
 
+    srand(time(NULL));
+    plc_split = MIN_LEAF_SIZE + rand() % (max - 1);     // random numbet between (MIN_LEAF_SIZE, max)
     if (splitH) {
+        l->left = leaf_create(l->x, l->y, l->width, plc_split);
+        l->right = leaf_create(l->x, l->y, l->width, l->height - plc_split);
+    } else {
+        l->left = leaf_create(l->x, l->y, plc_split, l->height);
+        l->right = leaf_create(l->x, l->y, l->width - plc_split, l->height);
+    }
 
+    return true;
+}
+
+void BSP_split(void) {
+    bool check_split = true;
+
+    cvector_push_back(lvec, leaf_root);
+    leaf_root = leaf_create(0, 0, WIDTH, HEIGHT);
+
+    while (check_split) {
+        size_t sizevec = cvector_get_size(lvec);
+        int rndnum;
+
+        check_split = false;
+        
+        for (size_t i = 0; i < sizevec; ++i) {
+            if (lvec[i]->left == NULL && lvec[i]->right == NULL) {
+                srand(time(NULL));
+                rndnum = 1 + rand() % 100;
+                if (lvec[i]->width > MAX_LEAF_SIZE || lvec[i]->height > MAX_LEAF_SIZE || rndnum < 75) {
+                    check_split = leaf_split(lvec[i]);
+
+                    if (check_split) {
+                        cvector_push_back(lvec, lvec[i]->left);
+                        cvector_push_back(lvec, lvec[i]->right);
+                    }
+                }
+            }
+        }
     }
 }
